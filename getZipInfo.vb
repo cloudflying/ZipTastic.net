3751bc023eb4496a7cc19d19dad73b1e8f58a6e7
Justin <justin@textconnects.com>
2012-08-27 10:04:33 -0400

    Added License Information

--- getZipInfo.vb
+++ getZipInfo.vb
@@ -4,6 +4,16 @@ Imports System.IO
 Imports Newtonsoft.Json
 
 Public Class getZipInfo
+    '
+    ' #### Ziptastic .net class library
+    ' #### Created by Justin Porter
+    ' #### Attribution 3.0 Unported (CC BY 3.0) 
+    ' #### License: http://creativecommons.org/licenses/by/3.0/
+    ' #### Please link to this article: http://www.stupidcodetricks.com/net-framework/intermediate/ziptastic-net-class-library/
+    ' #### view us on github: https://github.com/cloudflying/ZipTastic.net
+    ' #### Enjoy!
+
+
     ' in case of future upgrades
     Public API_Key As String = String.Empty
 
@@ -14,11 +24,11 @@ Public Class getZipInfo
         Catch ex As Exception
             Throw New ApplicationException("An error occurred retrieving your city and state information")
         End Try
-        
+
     End Function
 
     Private Function sendReq(zipCode As String, country As String) As String
-         Dim uri As String = "http://zip.elevenbasetwo.com/v2/" & country & "/" & zipCode
+        Dim uri As String = "http://zip.elevenbasetwo.com/v2/" & country & "/" & zipCode
         Dim request As HttpWebRequest = HttpWebRequest.Create(uri)
         request.Method = WebRequestMethods.Http.Get
         request.ContentType = "text/html"
